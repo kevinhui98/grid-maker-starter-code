a9cf20b1d2c2b81392c010c62dea31879a732b91
Kevin Hui <kevinhui98@gmail.com>
2022-10-17 20:17:01 -0400

    complete remove col

--- script.js
+++ script.js
@@ -31,7 +31,11 @@ function removeR() {
 
 // Remove a column
 function removeC() {
-    alert("Clicked Remove Col"); // Replace this line with your code.
+    // alert("Clicked Remove Col"); // Replace this line with your code.
+    let row = document.getElementById("row" + (numRows));
+    row.deleteCell(numCols - 1);
+    numCols--;
+    console.log(numCols);
 }
 
 // Set global variable for selected color
